d11ac970021f9a7d5c3f59c7afc2a11645551b29
Rodolfo Abrantes <rodolfo.abrantes@gmail.com>
2019-08-07 17:10:58 -0300

    Created component lista-de-tarefa-edit

---
+++ src/app/lista-de-presenca/lista-de-presenca-edit/lista-de-presenca-edit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-de-presenca-edit',
+  templateUrl: './lista-de-presenca-edit.component.html',
+  styleUrls: ['./lista-de-presenca-edit.component.css']
+})
+export class ListaDePresencaEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
