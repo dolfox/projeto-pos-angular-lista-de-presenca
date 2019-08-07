8c7ab6a29a000ba6ed741fdc7b395c5b8722a835
Rodolfo Abrantes <rodolfo.abrantes@gmail.com>
2019-08-07 16:54:57 -0300

    Created component list, implemented service.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaDePresencaListComponent } from './lista-de-presenca';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ListaDePresencaListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/lista-de-presenca/shered/lista-presenca.service.ts
+++ src/app/lista-de-presenca/shered/lista-presenca.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
 
+import { ListaDePresenca } from './lista-presenca';
+import { AngularFireDatabase } from '@angular/fire/database';
+import { map } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ListaPresencaService {
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
+
+  listAll(){
+    return this.db.list('lista-presenca')
+    .snapshotChanges()
+    .pipe(
+      map(changes => {
+        return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+      })
+    );
+  }
+
+  create(listaPresenca: ListaDePresenca): void{
+    this.db.list('lista-presenca').push(listaPresenca)
+    .then((result: any) => {
+      console.log(result.key);
+    });
+  }
+
+  findById(){
+
+  }
+
+  update(listaPresenca: ListaDePresenca, id: string): void{
+    this.db.list('lista-presenca').update(id, listaPresenca)
+    .catch((error: any) => {
+      console.log(error);
+    });
+  }
+
+  remove(id: string){
+    this.db.object('lista-presenca/${id}').remove();
+  }
+
+  changeStatus(){
+
+  }
 }
