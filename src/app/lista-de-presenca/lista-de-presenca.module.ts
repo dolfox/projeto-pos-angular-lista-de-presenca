634d1b6397b79500124272239060d26ba9835826
Rodolfo Abrantes <rodolfo.abrantes@gmail.com>
2019-08-07 17:31:04 -0300

    created insert component

--- src/app/lista-de-presenca/lista-de-presenca.module.ts
+++ src/app/lista-de-presenca/lista-de-presenca.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaDePresencaListComponent } from './lista-de-presenca-list/lista-de-presenca-list.component';
 import { ListaDePresencaEditComponent } from './lista-de-presenca-edit/lista-de-presenca-edit.component';
+import { ListaDePresencaCreateComponent } from './lista-de-presenca-create/lista-de-presenca-create.component';
 
 
 
 @NgModule({
-  declarations: [ListaDePresencaListComponent, ListaDePresencaEditComponent],
+  declarations: [ListaDePresencaListComponent, ListaDePresencaEditComponent, ListaDePresencaCreateComponent],
   imports: [
     CommonModule
   ]
