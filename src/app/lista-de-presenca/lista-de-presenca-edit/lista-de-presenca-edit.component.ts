3f3bc239f805e694957a9ffed2e7015105738a80
Rodolfo Abrantes <rodolfo.abrantes@gmail.com>
2019-08-08 16:54:53 -0300

    some corrections

--- src/app/lista-de-presenca/lista-de-presenca-edit/lista-de-presenca-edit.component.ts
+++ src/app/lista-de-presenca/lista-de-presenca-edit/lista-de-presenca-edit.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { ListaDePresenca, ListaPresencaService } from '../shered';
+import { ListaDePresenca, ListaPresencaService } from '../shared';
 import { Observable } from 'rxjs';
 
 @Component({
